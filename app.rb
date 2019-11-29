a8fbdb69a06cf96887d1a43be250033a7220fec2
Baktiar Miah <baktiar.m@gmail.com>
2019-11-29 21:49:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,10 +16,10 @@ end
 #   end
 #
 #   # Code your final two routes here:
-#  get '/goodbye/:name' do
-#    @user_name = params[:name]
-#    "Goodbye, #{@user_name}."
-#  end
+ get '/goodbye/:name' do
+   @user_name = params[:name]
+   "Goodbye, #{@user_name}."
+ end
 #
 # post '/multiply' do
 # end
